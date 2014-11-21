aae5099d377f0e49294858c6dce8cd2d65ebb6d9
Henning Schmiedehausen <henning@groupon.com>
2014-11-20 17:29:32 -0800

    fix 2

--- SingularityService/src/main/java/com/hubspot/singularity/mesos/SingularityStartup.java
+++ SingularityService/src/main/java/com/hubspot/singularity/mesos/SingularityStartup.java
@@ -21,12 +21,11 @@ import com.hubspot.singularity.SingularityDeployKey;
 import com.hubspot.singularity.SingularityPendingDeploy;
 import com.hubspot.singularity.SingularityTask;
 import com.hubspot.singularity.SingularityTaskHistoryUpdate;
-import com.hubspot.singularity.SingularityTaskIdHolder;
 import com.hubspot.singularity.SingularityTaskHistoryUpdate.SimplifiedTaskState;
 import com.hubspot.singularity.SingularityTaskId;
+import com.hubspot.singularity.SingularityTaskIdHolder;
 import com.hubspot.singularity.data.DeployManager;
 import com.hubspot.singularity.data.TaskManager;
-import com.hubspot.singularity.data.transcoders.Transcoder;
 import com.hubspot.singularity.data.zkmigrations.ZkDataMigrationRunner;
 import com.hubspot.singularity.scheduler.SingularityHealthchecker;
 import com.hubspot.singularity.scheduler.SingularityNewTaskChecker;
@@ -43,12 +42,11 @@ class SingularityStartup {
   private final SingularityHealthchecker healthchecker;
   private final SingularityNewTaskChecker newTaskChecker;
   private final DeployManager deployManager;
-  private final Transcoder<SingularityTask> taskTranscoder;
   private final SingularityTaskReconciliation taskReconciliation;
   private final ZkDataMigrationRunner zkDataMigrationRunner;
 
   @Inject
-  SingularityStartup(MesosClient mesosClient, Transcoder<SingularityTask> taskTranscoder, SingularityHealthchecker healthchecker, SingularityNewTaskChecker newTaskChecker,
+  SingularityStartup(MesosClient mesosClient, SingularityHealthchecker healthchecker, SingularityNewTaskChecker newTaskChecker,
       SingularitySlaveAndRackManager slaveAndRackManager, TaskManager taskManager, DeployManager deployManager, SingularityTaskReconciliation taskReconciliation,
       ZkDataMigrationRunner zkDataMigrationRunner) {
     this.mesosClient = mesosClient;
@@ -58,7 +56,6 @@ class SingularityStartup {
     this.newTaskChecker = newTaskChecker;
     this.taskManager = taskManager;
     this.healthchecker = healthchecker;
-    this.taskTranscoder = taskTranscoder;
     this.taskReconciliation = taskReconciliation;
   }
 
